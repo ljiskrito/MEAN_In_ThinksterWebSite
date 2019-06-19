6331f2ab783e130e42efcc2b4d8adcaa115c7c7d
ljiskrito <2311718442@qq.com>
2019-06-19 18:27:14 +0800

    add some styles-step2

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 
 var app = angular.module('flappernews', []);
 
-app.controller('MainCtrl',[
-    '$scope',
-    function($scope){
-        $scope.test = 'Hello World!';
+app.factory('posts', [
+    function(){
+        var o = {
+            posts: [{title: 'hello', link: '', upvotes: 0}]
+        };
+        return o;
     }
-]);
+])
+.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
+   
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function () {
+        if(!$scope.title || $scope.title === '') {return;}
+        $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0 });
+        $scope.title = '';
+        $scope.link = '';
+    };
+
+    $scope.increasePost = function (post) {
+        post.upvotes += 1;
+    }
+}]);
+
+
+
+
+
+
 
